7b6bfa5d91faca3f31c224acecda985416b86536
Elizeu RS <13ders@gmail.com>
2022-07-30 19:07:11 -0300

    assert that the response status is equal to ok | adding an expectation and an asynchronous wait  method | inject url string | prepare url

--- PhotoApp/PhotoApp/Signup/Network/SignupWebService.swift
+++ PhotoApp/PhotoApp/Signup/Network/SignupWebService.swift
@@ -9,7 +9,15 @@ import Foundation
 
 class SignupWebService {
   
+  private var urlString: String
+  
+  init(urlString: String) {
+    self.urlString = urlString
+  }
+  
   func signup(withForm formModel: SignupFormRequestModel, completionHandler: @escaping (SignupResponseModel?, SignupErrors?) -> Void) {
-    
+    guard let url = URL(string: urlString) else { return }
+    // TODO: Create a unit test to test that a specific error is returned is url is nil.
+    return
   }
 }
--- PhotoApp/PhotoAppTests/Network/SignupWebServiceTests.swift
+++ PhotoApp/PhotoAppTests/Network/SignupWebServiceTests.swift
@@ -20,15 +20,21 @@ class SignupWebServiceTests: XCTestCase {
   
   func testSignupWebService_WhenGivenSuccessfullResponse_ReturnsSuccess() {
     // Arrange
-    let sut = SignupWebService()
+    let sut = SignupWebService(urlString: "http://appsdeveloperblog.com:8080/signup-mock-service/users")
     
     let signFormRequestModel = SignupFormRequestModel(firstName: "Sergey", lastName: "Kargopolov", email: "test@test.com", password: "12345678")
+    
+    let expectation = self.expectation(description: "Signup Web Service Response Expectation")
     
     // Act
     sut.signup(withForm: signFormRequestModel) { (signupResponseModel,  error) in
       
       // Assert
-      
+      //"{\"status\":\"ok\"}"
+      XCTAssertEqual(signupResponseModel?.status, "ok")
+      expectation.fulfill()
     }
+    
+    self.wait(for: [expectation], timeout: 5)
   }
 }
