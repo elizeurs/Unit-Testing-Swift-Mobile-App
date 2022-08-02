5f06f05bb76463666f0a62a3f8d9082210364418
Elizeu RS <13ders@gmail.com>
2022-08-02 19:26:04 -0300

    create presenter unit test class and test method

---
+++ PhotoApp/PhotoAppTests/Signup/SignupPresenterTests.swift
@@ -0,0 +1,27 @@
+//
+//  SignupPresenterTests.swift
+//  PhotoAppTests
+//
+//  Created by Elizeu RS on 02/08/22.
+//
+
+import XCTest
+
+class SignupPresenterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+  
+  func testSignupPresenter_WhenInformationProvided_WillValidateEachProperty() {
+    // Arrange
+    
+    // Act
+    
+    // Assert
+  }
+}
