c86023fbfedcd4a1677374128b32bf9e0329e07c
Elizeu RS <13ders@gmail.com>
2022-08-03 17:21:26 -0300

    create validator protocol

--- PhotoApp/PhotoApp/Signup/Models/SignupFormModelValidator.swift
+++ PhotoApp/PhotoApp/Signup/Models/SignupFormModelValidator.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class SignupFormModelValidator {
+class SignupFormModelValidator: SignupModelValidatorProtocol {
   
   func isFirstNameValid(firstName: String) -> Bool {
     var returnValue = true
@@ -23,6 +23,30 @@ class SignupFormModelValidator {
 //    if firstName.isEmpty {
 //      returnValue = false
 //    }
+    
+    return returnValue
+  }
+  
+  func isLastNameValid(lastName: String) -> Bool {
+    var returnValue = true
+    
+    if lastName.count < SignupConstants.lastNameMinLength || lastName.count > SignupConstants.lastNameMaxLength {
+      returnValue = false
+    }
+    
+    return returnValue
+  }
+  
+  func isValidEmailFormat(email: String) -> Bool {
+    return NSPredicate(format: "SELF MATCHES%@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: email)
+  }
+  
+  func isPasswordValid(password: String) -> Bool {
+    var returnValue = true
+    
+    if password.count <  SignupConstants.passwordMinLength || password.count > SignupConstants.passwordMaxLength {
+      returnValue = false
+    }
     
     return returnValue
   }
