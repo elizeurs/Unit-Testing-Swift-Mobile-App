7125b48f55fcc6ce7488600b3403e1514a822ee1
Elizeu RS <13ders@gmail.com>
2022-08-08 10:03:50 -0300

    test uibutton has @ibaction assigned to it

--- PhotoApp/PhotoApp/Signup/Controllers/SignupViewController.swift
+++ PhotoApp/PhotoApp/Signup/Controllers/SignupViewController.swift
@@ -10,15 +10,11 @@ import UIKit
 class SignupViewController: UIViewController {
 
   @IBOutlet weak var firstNameTextField: UITextField!
-  
   @IBOutlet weak var lastNameTextField: UITextField!
-  
   @IBOutlet weak var emailTextField: UITextField!
-  
   @IBOutlet weak var passwordTextField: UITextField!
-  
   @IBOutlet weak var repeatPasswordTextField: UITextField!
-  
+  @IBOutlet weak var signupButton: UIButton!
   
   override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,15 +22,7 @@ class SignupViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  @IBAction func signupButtonTapped(_ sender: Any) {
+  }
 
 }
