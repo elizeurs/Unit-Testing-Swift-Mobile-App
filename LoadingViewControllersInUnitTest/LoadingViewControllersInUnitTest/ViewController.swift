8eddd25d8e89f413456593e4eebedb5892654d6b
Elizeu RS <13ders@gmail.com>
2022-08-09 19:26:08 -0300

    loading uiViewController that uses a storyBoard

---
+++ LoadingViewControllersInUnitTest/LoadingViewControllersInUnitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoadingViewControllersInUnitTest
+//
+//  Created by Elizeu RS on 09/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
