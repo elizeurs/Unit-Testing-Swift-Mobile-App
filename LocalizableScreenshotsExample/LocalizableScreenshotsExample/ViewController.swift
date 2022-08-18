216584d68b9bac0b9cfbfaf561c51ad5eddb837d
Elizeu RS <13ders@gmail.com>
2022-08-18 11:02:03 -0300

    Localization screenshots demo + Certificate

---
+++ LocalizableScreenshotsExample/LocalizableScreenshotsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalizableScreenshotsExample
+//
+//  Created by Elizeu RS on 17/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
